582634e1a5725fe94d6fa9f1d558b91bd1fe8cc8
nonemichal <michal.miler2001@gmail.com>
2025-09-13 16:47:12 +0200

    small fixes

--- lua/custom/plugins/remote-nvim.lua
+++ lua/custom/plugins/remote-nvim.lua
@@ -1,6 +1,5 @@
 return {
   'amitds1997/remote-nvim.nvim',
-  version = '*', -- Pin to GitHub releases
   dependencies = {
     'nvim-lua/plenary.nvim', -- For standard functions
     'MunifTanjim/nui.nvim', -- To build the plugin UI
